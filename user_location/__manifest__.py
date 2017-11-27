f0ee90836e44c7750ce44b02df230369181b9aa6
Odoo <odoo@aktivsoftware.com>
2017-11-27 18:05:43 +0530

    Initial commit for  User location for master branch.

---
+++ user_location/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'User Location',
+    'summary': 'Here users location is fetched',
+    'version': '10',
+    'author':"Aktiv Software",
+    'category': 'sale',
+    'depends': ['base'],
+    'website': 'www.aktivsoftware.com',
+    'data': [
+        'views/res_user.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
