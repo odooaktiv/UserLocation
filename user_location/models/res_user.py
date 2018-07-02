3566bdce67fccba66afb11fca58c4c29ecce5675
dsolanki <dhara.solanki@initos.com>
2018-07-02 12:09:59 +0530

    [IMP] Done required changes after testing.

--- user_location/models/res_user.py
+++ user_location/models/res_user.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 import requests
-import json
-import socket
-import datetime
-from geopy.geocoders import Nominatim
-from odoo.http import request
+
 
 class ResUserLog(models.Model):
     _inherit = 'res.users.log'
 
     location = fields.Char()
-    login_date = fields.Datetime(compute="_get_login_date", string="Login Date")
+    login_date = fields.Datetime(compute="_get_login_date",
+                                 string="Login Date")
 
     @api.multi
     def _get_login_date(self):
         for rec in self:
             rec.login_date = rec.create_date
+
 
 class Users(models.Model):
 
@@ -25,16 +23,16 @@ class Users(models.Model):
     @api.model
     def _update_last_login(self):
         vals = {}
-        ip = request.httprequest.environ.get('REMOTE_ADDR')
-        url = 'http://freegeoip.net/json/'+ip
+        url = 'http://ip-api.com/json/'
         r = requests.get(url)
         js = r.json()
-        geolocator = Nominatim(timeout=None)
-        a = js['latitude'], js['longitude']
-        location = geolocator.reverse(a)
-        user = self.search([('id', '=', self.id)])
-        if user and location:
-            vals.update({
-                'location' : location.address
-                })
-        self.env['res.users.log'].create(vals)
+        city = js['city']
+        regionname = js['regionName']
+        country = js['country']
+        address = city + ', ' + regionname + ', ' + country
+        vals.update({
+            'location': address,
+            'user_id': self.env.user.id})
+        user_log_id = self.env['res.users.log'].create(vals)
+        user = self.env.user
+        user.write({'log_ids': [(6, 0, [user_log_id.id])]})
