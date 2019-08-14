4445bedc9bd449b36f0e8e1c8f3baca63e255956
odooaktiv <odoo@aktivsoftware.com>
2019-08-14 14:25:09 +0530

    [UPDATE] solve bug which is comes from user location and commit it.

--- user_location/models/res_user.py
+++ user_location/models/res_user.py
@@ -23,13 +23,13 @@ class Users(models.Model):
     @api.model
     def _update_last_login(self):
         vals = {}
-        url = 'http://ip-api.com/json/'
+        url = 'http://ipinfo.io/json'
         r = requests.get(url)
         js = r.json()
         city = js['city']
-        regionname = js['regionName']
+        region = js['region']
         country = js['country']
-        address = city + ', ' + regionname + ', ' + country
+        address = city + ', ' + region + ', ' + country
         vals.update({
             'location': address,
             'user_id': self.env.user.id})
